ea380ec1a3406a9a1d85c551e7ee7056bafdb7a8
Alberto <sinnefa@gmail.com>
2020-01-19 12:18:21 +0100

    Added code to test performance

---
+++ performace_test.r
@@ -0,0 +1,36 @@
+library(profvis)
+rm(list=ls())
+size <- 100000
+values_in <- runif(size)
+
+## Standard approach
+profvis({
+  diff <- function(x, mu){
+    return <- (x-mu)^2
+  }
+  values <- c()
+  total <- 0
+  count <- 0 #assuming we don't know the stream length
+  # simulation values coming in
+  for (v in values_in) {
+    total <- total + v
+    values <- append(values,v)
+    count <- count + 1
+  }
+  mu <- total/count
+
+  num <- sum(sapply(values,diff,mu=mu))
+  sd <- sqrt(num/(size-1))
+})
+print(paste(mu,sd))
+
+#Welford
+profvis({
+  library(WelfordAlgorithm)
+  x <- WelfordAlgorithm$new()
+  # simulation values coming in
+  for (v in values_in) { 
+    x$push(v)
+  }
+})
+print(x)
